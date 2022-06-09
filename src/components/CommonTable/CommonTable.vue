940507e8c624d1f05dc17ec2537371dd654f0e01
郭正中 <guozhengzhong@tal.com>
2022-06-10 01:39:13 +0800

    add ag-table

---
+++ src/components/CommonTable/CommonTable.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="commonTableOuter">
+    <ag-grid-vue
+      style="width: 100%; height: 100%"
+      class="ag-theme-alpine"
+      :columnDefs="columnDefs"
+      :rowData="rowData"
+    ></ag-grid-vue>
+  </div>
+</template>
+
+<script>
+import { AgGridVue } from "@ag-grid-community/vue";
+import { ModuleRegistry } from "@ag-grid-community/core";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
+import "@ag-grid-community/core/dist/styles/ag-grid.css";
+import "@ag-grid-community/core/dist/styles/ag-theme-alpine.css";
+
+export default {
+  name: "CommonTable",
+  components: {
+    "ag-grid-vue": AgGridVue,
+  },
+  data() {
+    return {
+      columnDefs: [],
+      rowData: [],
+    };
+  },
+  beforeMount() {
+    this.columnDefs = [
+      { field: "make" },
+      { field: "model" },
+      { field: "price", sortable: true },
+    ];
+    this.rowData = [
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxster", price: 72000 },
+    ];
+  },
+};
+</script>
+
+<style scoped>
+.commonTableOuter {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/Table/AgTable/AgTable.vue
+++ src/views/Table/AgTable/AgTable.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>ag-table</div>
+  <div class="agTableOuter">
+    <CommonTable />
+  </div>
 </template>
-<style scoped></style>
+<style scoped>
+.agTableOuter {
+  height: calc(100vh - 53px - 16px - 64px - 24px - 28px);
+}
+</style>
 <script>
+import CommonTable from "@/components/CommonTable/CommonTable.vue";
 export default {
   name: "AgTable",
+  components: {
+    CommonTable,
+  },
 };
 </script>
